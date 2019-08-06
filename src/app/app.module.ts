d9864ed2b9eed4a315df25edc80744963b10caa2
ScottMcgowan20 <scoott@vt.edu>
2019-08-05 22:28:01 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { AppComponent } from './home/app.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-import { AppComponent } from './app.component';
+const appRoutes: Routes = [
+  {
+    path:'',
+    redirectTo: '/about-me',
+    pathMatch: 'full'
+  },
+  {
+    path:'hobbies',
+    component: HobbiesComponent
+  },
 
+  {
+    path:"about-me",
+    component: AboutMeComponent
+  },
+
+  {
+    path:"experience",
+    component: ExperienceComponent
+  },
+  
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HobbiesComponent,
+    AboutMeComponent,
+    ExperienceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( appRoutes ),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private router: Router) {
+
+  }
+}
